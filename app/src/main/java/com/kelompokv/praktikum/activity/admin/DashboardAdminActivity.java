08bae8c4914c63c7587f7a801bc9737f5d97111e
Devmptr <dyevamp46@gmail.com>
2020-12-11 00:23:29 +0700

    refactor (admin) : tambah view dashboard admin

---
+++ app/src/main/java/com/kelompokv/praktikum/activity/admin/DashboardAdminActivity.java
@@ -0,0 +1,16 @@
+package com.kelompokv.praktikum.activity.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kelompokv.praktikum.R;
+
+public class DashboardAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_admin);
+    }
+}
